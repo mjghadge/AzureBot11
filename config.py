01f8b73b749c1276303f679d3ae4f8a8cd782f23
Mayur Ghadge <94392152+mjghadge@users.noreply.github.com>
2022-12-13 15:39:52 +0530

    User credentials removed

--- config.py
+++ config.py
@@ -8,5 +8,5 @@ class DefaultConfig:
     """ Bot Configuration """
 
     PORT = 3978
-    APP_ID = os.environ.get("MicrosoftAppId", "b54bf997-b017-41ab-92ca-3e516cf18dfb")
-    APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "Aev_AJ96~XU-53Zj26rVaFN.rh_t87Wqto")
+    APP_ID = os.environ.get("MicrosoftAppId", "")
+    APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "")
